c91983f851b815bb74285e048dedc08fbe04ff92
benji703 <benjadcool@gmail.com>
2023-04-10 12:11:05 +0200

    WiP: integration with MQTT

--- Arduino/TempSens/TempSens.ino
+++ Arduino/TempSens/TempSens.ino
@@ -6,15 +6,23 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+String deviceName = "M1";
+
+
+//MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+const char* mqtt_server = "192.168.204.26";
 
 // GPIO where the DS18B20 is connected to
 const int oneWireBus = 33;  
 float sampleRate = 1; //Samples per second
 float interval = 1000/sampleRate;
 
-const int initialBatchSize = 50;
+const int initialBatchSize = 10;
 
 int batchSize = initialBatchSize;
 int batchNum = 0;
@@ -42,10 +50,18 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
   
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
   if (batchNum > batchSize-1) {
     Serial.println("Send batch");
     for (int i = 0; i < batchNum; i++) {
@@ -59,33 +75,99 @@ void loop() {
   batchNum = batchNum + 1;
   
   delay(interval); //Amount of miliseconds between each meassurement
-  setSampleRate(sampleRate+1);
-
+  
   
 }
 
 void setSampleRate(float samp) {
   sampleRate = samp;
   interval = 1000/sampleRate;
+
+  Serial.print("New samplingrate: ");
+  Serial.println(samp);
 }
 
 void setBatchSize(int newBatchSize) {
   batchSize = newBatchSize;
   //TODO: Send current measurements
+  Serial.print("New batch size: ");
+  Serial.println(newBatchSize);
   
   batchNum = 0;
 }
 
 void sendBatch() {
-  HTTPClient http;
-  http.begin("0.0.0.0");
-  http.addHeader("Content-Type", "application/json");
+  
+}
 
-  int httpCode = http.POST();
 
-  if (httpCode != HTTP_CODE_OK) {
-     
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("sensor/config");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
+}
 
-  http.end();
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "sensor/config") {
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, messageTemp);
+
+    int newBatchSize;
+    int newSampleRate;
+
+    for (int i = 0; i < sizeof(doc); i++) {
+      if (doc[i] == deviceName) {
+        newBatchSize = doc[i]["sensors"][0]["settings"]["batchSize"];
+        newSampleRate = doc[i]["sensors"][0]["settings"]["samplingRate"];
+      }
+    }
+    
+    setSampleRate(newSampleRate);
+    setBatchSize(newBatchSize);
+  }
+}
+
+void json() {
+  String jsonString = "[     {         \"name\": \"m1\",         \"sensors\": [             {                 \"name\": \"s1\",                 \"settings\": {                     \"samplingRate\": 100.0,                     \"batchSize\": 3000                 }             },             {                 \"name\": \"s2\",                 \"settings\": {                     \"samplingRate\": 3.3333333,                     \"batchSize\": 1000                 }             }         ]     },     {         \"name\": \"m2\",         \"sensors\": [             {                 \"name\": \"s1\",                 \"settings\": {                     \"samplingRate\": 1666.6666,                     \"batchSize\": 300                 }             }         ]     } ]";
+  
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, jsonString);
+
+  String nameM2 = doc[1]["name"];
+  float sampleM2S1 = doc[1]["sensors"][0]["settings"]["samplingRate"];
+  int batchM2S1 = doc[1]["sensors"][0]["settings"]["batchSize"];
+
+  Serial.println(nameM2);
+  Serial.println(sampleM2S1);
+  Serial.println(batchM2S1);
 }
