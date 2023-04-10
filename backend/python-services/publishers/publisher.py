9b206261ce07a160f686f3932178566fe8c5ccc5
alpiha <alpiha1515@gmail.com>
2023-04-10 08:14:57 +0200

    restucture

---
+++ backend/python-services/publishers/publisher.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt 
+#from random import uniform
+import time
+import json
+
+#mqttBroker = "mqtt.eclipseprojects.io"
+#client.connect(mqttBroker)
+client = mqtt.Client("configuration_publisher")
+client.connect("127.0.0.1", 1883)
+
+with open('./resources/configuration.json','r') as f:
+    config = json.load(f)
+
+json_message = json.dumps(config)
+
+while True:
+    #randNumber = uniform(20.0, 21.0)
+    client.publish("sensor/temperature", json_message)
+    print("backend just published: " + str(json_message) + " to Topic: sensor/temperature")
+    time.sleep(1)
