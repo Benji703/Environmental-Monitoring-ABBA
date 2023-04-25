c0d632f4253fdc30f123c1c76c6931a466299129
alpiha <alpiha1515@gmail.com>
2023-04-25 14:02:14 +0200

    add: heartbeat consumer & refactor it

---
+++ backend/python-services/subscribers/heartbeat/heartbeat.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt 
+from flask import Flask, request, jsonify
+import requests
+import time
+import json
+
+app = Flask(__name__)
+headers = {"Content-Type": "application/json; charset=utf-8"}
+url = 'http://client:3000/receive-json'
+
+def on_message(client, userdata, message):
+    parsed_json = json.loads(message.payload.decode("utf-8"))
+   # response = requests.post( url=url, headers=headers, json=parsed_json)
+    #return response
+    print("The backend received message: ", str(message.payload.decode("utf-8")))
+
+client = mqtt.Client("heartbeat_consumer")
+client.connect("mosquitto", 1883)
+
+client.loop_start()
+client.subscribe("heartbeat")
+client.on_message = on_message
+time.sleep(1000)
+client.loop_end()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6000)
