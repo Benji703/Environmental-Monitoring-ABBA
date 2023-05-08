c26db32c961df495182ab60a2089dfeafa955651
alpiha <alpiha1515@gmail.com>
2023-05-08 12:27:33 +0200

    Bugfix: communication with backend client

--- Backend/Python-services/Subscribers/Heartbeat/heartbeat.py
+++ Backend/Python-services/Subscribers/Heartbeat/heartbeat.py
@@ -9,22 +9,25 @@ app = Flask(__name__)
 headers = {"Content-Type": "application/json; charset=utf-8"}
 url = 'http://client:3000/receive-heartbeats'
 
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("heartbeats")
+    
+
 def on_message(client, userdata, message):
     parsed_json = json.loads(message.payload.decode("utf-8"))
     timestamp = int(time.time())
     date = datetime.fromtimestamp(timestamp).isoformat()
-    heartbeat = {"machine id": parsed_json["machine_id"], "timestamp": date}
+    heartbeat = {"machine_id": parsed_json, "timestamp": date}
+    print(heartbeat)
     response = requests.post( url=url, headers=headers, json=heartbeat)
     return response
 
 client = mqtt.Client("heartbeat_consumer")
+client.on_connect = on_connect
+client.on_message = on_message
 client.connect("mosquitto", 1883)
 
-client.loop_start()
-client.subscribe("heartbeats")
-client.on_message = on_message
-time.sleep(1000)
-client.loop_end()
+client.loop_forever()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=6000)
--- Backend/Python-services/Subscribers/Temperature/temperature.py
+++ Backend/Python-services/Subscribers/Temperature/temperature.py
@@ -8,20 +8,22 @@ app = Flask(__name__)
 headers = {"Content-Type": "application/json; charset=utf-8"}
 url = 'http://client:3000/receive-temperature'
 
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("sensor/temperature")
+    
+
 def on_message(client, userdata, message):
+    print("The backend received message: ", str(message.payload.decode("utf-8")))
     parsed_json = json.loads(message.payload.decode("utf-8"))
     response = requests.post( url=url, headers=headers, json=parsed_json)
     return response
-    #print("The backend received message: ", str(message.payload.decode("utf-8")))
 
-client = mqtt.Client("temperature_consumer")
+client = mqtt.Client("temperaturee_consumer")
+client.on_connect = on_connect
+client.on_message = on_message
 client.connect("mosquitto", 1883)
 
-client.loop_start()
-client.subscribe("sensor/temperature")
-client.on_message = on_message
-time.sleep(1000)
-client.loop_end()
+client.loop_forever()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
